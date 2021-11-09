98e906cf93fcf3177642595350f8317d37c92711
jatin <jatinyadav0000@gmail.com>
2021-11-09 21:11:33 +0530

    Initial commit

---
+++ taskpy.py
@@ -0,0 +1,20 @@
+import sys
+betty_botter = 'Betty Botter bought a bit of butter But the bit of butter Betty Botter bought was bitter So Betty ' \
+                'Botter bought a better bit of butter '
+
+expected_output = 'botter betty bought a bit of butter But the bit of butter botter betty bought was bitter So botter ' \
+ 'betty bought a better bit of butter '
+
+def botter_betty():
+    betty_botter = 'Betty Botter bought a bit of butter But the bit of butter Betty Botter bought was bitter So Betty ' \
+    'Botter bought a better bit of butter '
+    betty_botter = betty_botter.replace('Botter', 'betty')
+    betty_botter = betty_botter.replace('Betty', 'botter')
+    return betty_botter
+
+def main(args):
+    betty_botter=botter_betty()
+    assert betty_botter == expected_output, "Failed"
+    print("Success")
+if __name__ == '__main__':
+ main(sys.argv)
